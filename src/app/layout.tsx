17763da12934ef096b4f333a8ae0d56328761059
Klecianny Melo <klecianny.melo@quorum.us>
2025-07-15 21:50:24 -0300

    :lipstick: code and github profile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         <footer className="mx-auto flex max-w-prose flex-col max-sm:items-start items-center gap-2 py-6 text-sm text-zinc-500 dark:text-zinc-400">
           <div className="flex items-center gap-4">
             <a
-              className="decoration-zinc-500 underline-offset-4 transition-all sm:hover:underline dark:decoration-zinc-400"
+              className="decoration-[#228b22] underline-offset-4 transition-all hover:underline hover:text-[#228b22] hover:font-bold dark:decoration-[#3a86ff] dark:hover:text-[#3a86ff]"
               href="https://github.com/Kecbm/blog"
               target="_blank"
             >
@@ -49,7 +49,7 @@ export default function RootLayout({
             </a>
 
             <a
-              className="decoration-zinc-500 underline-offset-4 transition-all sm:hover:underline dark:decoration-zinc-400"
+              className="decoration-[#228b22] underline-offset-4 transition-all hover:underline hover:text-[#228b22] hover:font-bold dark:decoration-[#3a86ff] dark:hover:text-[#3a86ff]"
               href="https://github.com/Kecbm"
               target="_blank"
             >
