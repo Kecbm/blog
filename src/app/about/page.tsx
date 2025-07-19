41aa2b06ddeba776fa2b567afcc409882aff8bf5
Klecianny Melo <klecianny.melo@quorum.us>
2025-07-19 15:48:59 -0300

    :lipstick: about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ export default function AboutPage() {
           I&apos;m an AI-First Software Engineer based in{" "}
           <a
             href="https://pt.wikipedia.org/wiki/Garanhuns"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             Garanhuns
           </a>, Pernambuco, Brazil.
@@ -19,7 +19,7 @@ export default function AboutPage() {
           Before working in tech, I spent 10 years as a professional musician, playing the saxophone in various groups, including the{" "}
           <a
             href="https://www.facebook.com/share/p/1bCxoDSPSc/"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             Manoel Rabelo Orchestra
           </a>.
@@ -28,7 +28,7 @@ export default function AboutPage() {
           I hold a degree in{" "}
           <a
             href="https://www.instagram.com/engenhariadealimentos.ufape/"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             Federal University of the Agreste of Pernambuco
           </a>. It was during the core engineering courses that I had my first contact with programming logic, and I even became a teaching assistant for the Algorithms and Computer Programming course.
@@ -37,25 +37,25 @@ export default function AboutPage() {
           In 2021, I began my career transition into technology. I completed the{" "}
           <a
             href="https://www.betrybe.com/"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             Full Stack Web Development Bootcamp at Trybe{" "}
           </a>in the{" "}
           <a
             href=""
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             XP Class
           </a>, where I served as an{" "}
           <a
             href="https://www.linkedin.com/posts/kecbm_tive-a-honra-de-ser-eleita-embaixatryber-activity-6905503717567598592-4Wrg?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAACtMpX0BMkf2Xh5O6zP34bZB84vMCGPU1YY"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             ambassador
           </a>{" "}and participated in the series{" "}
           <a
             href="https://www.youtube.com/watch?v=Tonfpy4eQFY&list=PLw0GGb7tHTHv3gdy39NrOrLh7TrnqjUrL"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             Beyond the Code.
           </a>
@@ -64,7 +64,7 @@ export default function AboutPage() {
           Even before joining Trybe, I started researching on Twitter what it was like to work as a developer. Since then, I’ve been active in the{" "}
           <a
             href="https://dev.to/kecbm/como-participar-da-bolha-dev-no-twitter-um-guia-para-desenvolvedores-11ol"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             Dev Bubble
           </a>, a vibrant community where I continuously learn, share my journey, and connect with amazing people in the field.
@@ -73,7 +73,7 @@ export default function AboutPage() {
           Being part of this community opened up incredible opportunities, including being{" "}
           <a
             href="https://g1.globo.com/tecnologia/noticia/2023/05/10/ja-incentivo-meus-filhos-comeco-pode-ser-frustrante-profissionais-contam-como-e-trabalhar-com-programacao.ghtml"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             interviewed by Globo
           </a>, where I talked about my experience going through my first layoff.
@@ -82,7 +82,7 @@ export default function AboutPage() {
           As a content creator on{" "}
           <a
             href="https://dev.to/kecbm"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             DEV Community
           </a>, I write about career development, problem-solving, and personal growth in tech. I’ve published over 24 articles, with more than 14,300 views and 3,600 followers.
@@ -91,13 +91,13 @@ export default function AboutPage() {
           My most impactful article documents everything that helped me quickly{" "}
           <a
             href="https://dev.to/kecbm"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             get back into the tech market after the layoff
           </a>, and I had the honor of presenting its key takeaways at{" "}
           <a
             href="https://www.twitch.tv/videos/2435200518"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             GitHub Brasil
           </a>.
@@ -106,13 +106,13 @@ export default function AboutPage() {
           I have seven pets: one pinscher and six cats. In my free time, I enjoy playing PS3, riding my{" "}
           <a
             href="https://www.strava.com/athletes/kecbm"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             bike
           </a>, going to the{" "}
           <a
             href="https://www.strava.com/athletes/kecbm"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             gym
           </a>, and maintaining a daily reading habit.
@@ -121,13 +121,13 @@ export default function AboutPage() {
           If you want to get in touch, send me a DM on{" "}
           <a
             href="https://x.com/kecbm"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             Twitter
           </a> or an email{" "}
           <a
             href="mailto:kleciannymelo@gmail.com"
-            className="underline decoration-[#8338ec] underline-offset-4 transition-all hover:text-[#8338ec] hover:font-bold dark:decoration-[#ff006e] dark:hover:text-[#ff006e]"
+            className="about-highlight"
           >
             kleciannymelo@gmail.com
           </a>.
