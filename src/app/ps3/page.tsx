e6823c99696149f0e503d7344c59613e9dcde95c
Klecianny Melo <klecianny.melo@quorum.us>
2025-07-22 20:51:29 -0300

    :hammer: ps3 page

---
+++ src/app/ps3/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+
+interface PS3Game {
+  name: string;
+  imageUrl: string;
+}
+
+const ps3Games: PS3Game[] = [
+  {
+    name: "Gran Turismo 6",
+    imageUrl: "https://m.media-amazon.com/images/I/714eNHubwSL._UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "Guitar Hero III",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/pt/b/be/Guitar_Hero_3_capa.png",
+  },
+  {
+    name: "FIFA 19",
+    imageUrl: "https://m.media-amazon.com/images/I/813OLtvhFaL.jpg",
+  },
+];
+
+function PS3GameCard({ name, imageUrl }: PS3Game) {
+  return (
+    <div className="flex flex-col overflow-hidden rounded ring-1 ring-zinc-400 dark:ring-zinc-500 h-[324px]">
+      <Image
+        src={imageUrl}
+        width={310}
+        height={324}
+        alt={`Cover for ${name}`}
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+}
+
+export default function PS3Page() {
+  return (
+    <>
+      <h1 className="mb-16 mt-4 text-center text-5xl max-sm:text-4xl">
+        PS3
+      </h1>
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6">
+        {ps3Games.map((game) => (
+          <PS3GameCard key={game.name} {...game} />
+        ))}
+      </div>
+    </>
+  );
+}
