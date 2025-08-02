c7c2132cd6082be04ad750866477d300ed41fa25
Klecianny Melo <klecianny.melo@quorum.us>
2025-08-02 13:14:38 -0300

    :sparkles: ps3 page

--- src/app/ps3/page.tsx
+++ src/app/ps3/page.tsx
@@ -1,35 +1,50 @@
 import Image from "next/image";
 import FloatingIcons from "../../components/FloatingIcons";
 import { PlayStationIcons } from "../../components/PlayStationIcons";
-import GameStatusBadge from "../../components/GameStatusBadge";
+import GameStatusBadge from "../../components/StatusBadge";
 
 interface PS3Game {
   name: string;
   imageUrl: string;
-  status: "in-progress" | "done";
+  status: "in-progress" | "done" | "pending";
 }
 
 const ps3Games: PS3Game[] = [
   {
     name: "Gran Turismo 6",
     imageUrl: "https://m.media-amazon.com/images/I/714eNHubwSL._UF1000,1000_QL80_.jpg",
-    status: "done",
+    status: "in-progress",
   },
   {
     name: "Guitar Hero III",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/pt/b/be/Guitar_Hero_3_capa.png",
+    imageUrl: "https://m.media-amazon.com/images/I/81fa9l6X9aL.jpg",
     status: "in-progress",
   },
   {
     name: "FIFA 19",
     imageUrl: "https://m.media-amazon.com/images/I/813OLtvhFaL.jpg",
-    status: "done",
+    status: "in-progress",
+  },
+  {
+    name: "The Last Of Us",
+    imageUrl: "https://m.media-amazon.com/images/I/A1dXfW1yPNL.jpg",
+    status: "pending",
+  },
+  {
+    name: "God Of War",
+    imageUrl: "https://m.media-amazon.com/images/I/71DG2S+Y+DL.jpg",
+    status: "pending",
+  },
+  {
+    name: "GTA 5",
+    imageUrl: "https://m.media-amazon.com/images/I/71FVWhz78fL._UF894,1000_QL80_.jpg",
+    status: "pending",
   },
 ];
 
 function PS3GameCard({ name, imageUrl, status }: PS3Game) {
   return (
-    <div className="flex flex-col overflow-hidden rounded ring-1 ring-zinc-400 dark:ring-zinc-500 h-[324px] relative">
+    <div className="flex flex-col overflow-hidden rounded ring-1 ring-zinc-400 dark:ring-zinc-500 h-[324px] w-[281px] relative">
       <Image
         src={imageUrl}
         width={310}
