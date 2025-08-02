ed3817962beee5244ed80e82a8f10692d93b2e74
Klecianny Melo <klecianny.melo@quorum.us>
2025-08-02 14:12:14 -0300

    :sparkles: books page in the not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,21 +5,28 @@ export default function NotFound() {
         This page doesn&apos;t exist.
       </h1>
       <p className="text-sm">
-        Oops! Looks like you took the wrong turn. You can head back to the{" "}
+        Oops! It looks like you’ve taken a wrong turn. No worries, you can head back to the{" "}
         <a
           href="/"
           className="notfound-highlight"
         >
           Home
         </a>{" "}
-        page, or stick around and check out the{" "}
+        page, or if you’re curious, check out the{" "}
         <a
           href="/ps3"
           className="notfound-highlight"
         >
           PS3
         </a>{" "}
-        games I&apos;m currently playing just below!
+        games I&apos;ve been enjoying lately or the{" "}
+        <a
+          href="/books"
+          className="notfound-highlight"
+        >
+          books
+        </a>{" "}
+        I&apos;m currently reading!
       </p>
     </div>
   );
