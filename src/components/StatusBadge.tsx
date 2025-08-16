2d745fb95dd69b09445a53b597b87152bd63693f
Klecianny Melo <klecianny.melo@quorum.us>
2025-08-16 17:30:25 -0300

    :lipstick: new colors for dark mode in the status badge

--- src/components/StatusBadge.tsx
+++ src/components/StatusBadge.tsx
@@ -8,7 +8,7 @@ interface StatusBadgeProps {
 export default function StatusBadge({ status, inProgressLabel = "Playing" }: StatusBadgeProps) {
   if (status === "done") {
     return (
-      <div className="absolute top-3 right-3 flex items-center gap-1.5 bg-[#68a60a]/80 px-2.5 py-1.5 rounded-full text-xs font-bold shadow-lg dark:bg-[#88d012]/80 text-[#1b2f04] dark:text-[#385512]">
+      <div className="absolute top-3 right-3 flex items-center gap-1.5 bg-[#68a60a]/80 px-2.5 py-1.5 rounded-full text-xs font-bold shadow-lg dark:bg-[#88d012]/80 text-[#1b2f04]">
         <Check size={14} strokeWidth={3}/>
         <span>Completed</span>
       </div>
@@ -17,7 +17,7 @@ export default function StatusBadge({ status, inProgressLabel = "Playing" }: Sta
 
   if (status === "in-progress") {
     return (
-      <div className="absolute top-3 right-3 flex items-center gap-1.5 bg-[#c59009]/80 px-2.5 py-1.5 rounded-full text-xs font-bold shadow-lg dark:bg-[#e4b90e]/80 text-[#402208] dark:text-[#6e4215]">
+      <div className="absolute top-3 right-3 flex items-center gap-1.5 bg-[#c59009]/80 px-2.5 py-1.5 rounded-full text-xs font-bold shadow-lg dark:bg-[#e4b90e]/80 text-[#402208]">
         <Clock size={14}/>
         <span>{inProgressLabel}</span>
       </div>
@@ -26,7 +26,7 @@ export default function StatusBadge({ status, inProgressLabel = "Playing" }: Sta
 
   if (status === "pending") {
     return (
-      <div className="absolute top-3 right-3 flex items-center gap-1.5 bg-[#7f87ac]/80 dark:bg-[#96a0be]/80 text-[#23252e] px-2.5 py-1.5 rounded-full text-xs font-bold shadow-lg transition-colors text-[#2c2e3a] dark:text-[#2c2e3a]">
+      <div className="absolute top-3 right-3 flex items-center gap-1.5 bg-[#7f87ac]/80 dark:bg-[#96a0be]/80 text-[#23252e] px-2.5 py-1.5 rounded-full text-xs font-bold shadow-lg transition-colors text-[#2c2e3a]">
         <Hourglass size={14}/>
         <span>Pending</span>
       </div>
