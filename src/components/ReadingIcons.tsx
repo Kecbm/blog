29117ae0bedac6829567e8ac6b6d08762ce807ca
Klecianny Melo <klecianny.melo@quorum.us>
2025-10-18 12:30:06 -0300

    :sparkles: reading icons

---
+++ src/components/ReadingIcons.tsx
@@ -0,0 +1,32 @@
+import { BookOpen, GraduationCap, Lightbulb, Monitor } from "lucide-react";
+
+export const ReadingIcons = [
+  <BookOpen
+    key="book"
+    className="text-[#68a60a] dark:text-[#acf328]"
+    size={24}
+    fill="none"
+    strokeWidth={2}
+  />,
+  <GraduationCap
+    key="graduation"
+    className="text-[#68a60a] dark:text-[#acf328]"
+    size={24}
+    fill="none"
+    strokeWidth={2}
+  />,
+  <Lightbulb
+    key="lightbulb"
+    className="text-[#68a60a] dark:text-[#acf328]"
+    size={24}
+    fill="none"
+    strokeWidth={2}
+  />,
+  <Monitor
+    key="computer"
+    className="text-[#68a60a] dark:text-[#acf328]"
+    size={24}
+    fill="none"
+    strokeWidth={2}
+  />,
+];
