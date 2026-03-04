7dea2f094722c806664a95bcce3d36f47ac2fd46
Klecianny Melo <klecianny.melo@quorum.us>
2026-03-04 10:51:05 -0300

    :sparkles: about with translations

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,141 +1,147 @@
+"use client";
+
+import { useTranslation } from "@/src/hooks/useTranslation";
+
 export default function AboutPage() {
+  const { t } = useTranslation();
+
   return (
     <>
       <h1 className="mb-16 mt-4 text-center text-5xl max-sm:text-4xl">
-        About me
+        {t.about.title}
       </h1>
       <article>
         <p>
-          I&apos;m an AI-First Software Engineer based in{" "}
+          {t.about.paragraph1}{" "}
           <a
             href="https://pt.wikipedia.org/wiki/Garanhuns"
             className="about-highlight"
           >
-            Garanhuns
-          </a>, Pernambuco, Brazil.
+            {t.about.garanhuns}
+          </a>{t.about.paragraph1End}
         </p>
         <p>
-          Before working in tech, I spent 10 years as a professional musician, playing the saxophone in various groups, including the{" "}
+          {t.about.paragraph2}{" "}
           <a
             href="https://www.facebook.com/share/p/1bCxoDSPSc/"
             className="about-highlight"
           >
-            Manoel Rabelo Orchestra
+            {t.about.orchestra}
           </a>.
         </p>
         <p>
-          I hold a degree in{" "}
+          {t.about.paragraph3Start}{" "}
           <a
             href="https://www.instagram.com/engenhariadealimentos.ufape/"
             className="about-highlight"
           >
-            Federal University of the Agreste of Pernambuco
-          </a>. It was during the core engineering courses that I had my first contact with programming logic, and I even became a teaching assistant for the Algorithms and Computer Programming course.
+            {t.about.university}
+          </a>{t.about.paragraph3End}
         </p>
         <p>
-          In 2021, I began my career transition into technology. I completed the{" "}
+          {t.about.paragraph4Start}{" "}
           <a
             href="https://www.betrybe.com/"
             className="about-highlight"
           >
-            Full Stack Web Development Bootcamp at Trybe{" "}
-          </a>in the{" "}
+            {t.about.trybe}{" "}
+          </a>{t.about.paragraph4Middle}{" "}
           <a
             href="https://www.infomoney.com.br/patrocinados/xp-investimentos/xp-inc-vai-formar-300-pessoas-em-curso-de-programacao-da-trybe/"
             className="about-highlight"
           >
-            XP Class
-          </a>, where I served as an{" "}
+            {t.about.xpClass}
+          </a>{t.about.paragraph4Middle2}{" "}
           <a
             href="https://www.linkedin.com/posts/kecbm_tive-a-honra-de-ser-eleita-embaixatryber-activity-6905503717567598592-4Wrg?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAACtMpX0BMkf2Xh5O6zP34bZB84vMCGPU1YY"
             className="about-highlight"
           >
-            ambassador
-          </a>{" "}and participated in the series{" "}
+            {t.about.ambassador}
+          </a>{" "}{t.about.paragraph4Middle3}{" "}
           <a
             href="https://www.youtube.com/watch?v=Tonfpy4eQFY&list=PLw0GGb7tHTHv3gdy39NrOrLh7TrnqjUrL"
             className="about-highlight"
           >
-            Beyond the Code.
+            {t.about.beyondCode}
           </a>
         </p>
         <p>
-          Even before joining Trybe, I started researching on Twitter what it was like to work as a developer. Since then, I’ve been active in the{" "}
+          {t.about.paragraph5Start}{" "}
           <a
             href="https://dev.to/kecbm/como-participar-da-bolha-dev-no-twitter-um-guia-para-desenvolvedores-11ol"
             className="about-highlight"
           >
-            Dev Bubble
-          </a>, a vibrant community where I continuously learn, share my journey, and connect with amazing people in the field.
+            {t.about.devBubble}
+          </a>{t.about.paragraph5End}
         </p>
         <p>
-          Being part of this community opened up incredible opportunities, including being{" "}
+          {t.about.paragraph6Start}{" "}
           <a
             href="https://g1.globo.com/tecnologia/noticia/2023/05/10/ja-incentivo-meus-filhos-comeco-pode-ser-frustrante-profissionais-contam-como-e-trabalhar-com-programacao.ghtml"
             className="about-highlight"
           >
-            interviewed by Globo
-          </a>, where I talked about my experience going through my first layoff.
+            {t.about.globo}
+          </a>{t.about.paragraph6End}
         </p>
         <p>
-          As a content creator on{" "}
+          {t.about.paragraph7Start}{" "}
           <a
             href="https://dev.to/kecbm"
             className="about-highlight"
           >
-            DEV Community
-          </a>, I write about career development, problem-solving, and personal growth in tech. I’ve published over 24 articles, with more than 14,300 views and 3,600 followers.
+            {t.about.devCommunity}
+          </a>{t.about.paragraph7End}
         </p>
         <p>
-          My most impactful article documents everything that helped me quickly{" "}
+          {t.about.paragraph8Start}{" "}
           <a
             href="https://dev.to/kecbm/de-volta-ao-codigo-como-conquistei-minha-realocacao-no-mercado-de-tecnologia-4n5n"
             className="about-highlight"
           >
-            get back into the tech market after the layoff
-          </a>, and I had the honor of presenting its key takeaways at{" "}
+            {t.about.backToCode}
+          </a>{t.about.paragraph8Middle}{" "}
           <a
             href="https://www.twitch.tv/videos/2435200518"
             className="about-highlight"
           >
-            GitHub Brasil
+            {t.about.githubBrasil}
           </a>.
         </p>
         <p>
-          I have nine pets: one pinscher and eight cats. In my free time, I enjoy playing{" "}
+          {t.about.paragraph9Start}{" "}
           <a
             href="/ps3"
             className="about-highlight"
           >
-            PS3
-          </a>, riding my{" "}
+            {t.about.ps3}
+          </a>{t.about.paragraph9Middle}{" "}
           <a
             href="https://www.strava.com/athletes/kecbm"
             className="about-highlight"
           >
-            bike
-          </a>, going to the{" "}
+            {t.about.bike}
+          </a>{t.about.paragraph9Middle2}{" "}
           <a
             href="https://www.strava.com/athletes/kecbm"
             className="about-highlight"
           >
-            gym
-          </a>, and maintaining a daily{" "}
+            {t.about.gym}
+          </a>{t.about.paragraph9Middle3}{" "}
           <a
             href="/books"
             className="about-highlight"
           >
-            reading
-          </a> habit.
+            {t.about.reading}
+          </a>{t.about.paragraph9End}
         </p>
         <p>
-          If you want to get in touch, send me a DM on{" "}
+          {t.about.paragraph10Start}{" "}
           <a
             href="https://x.com/kecbm"
             className="about-highlight"
           >
-            Twitter
-          </a> or an email{" "}
+            {t.about.twitter}
+          </a>{" "}{t.about.paragraph10Middle}{" "}
           <a
             href="mailto:kleciannymelo@gmail.com"
             className="about-highlight"
@@ -143,9 +149,9 @@ export default function AboutPage() {
             kleciannymelo@gmail.com
           </a>.
         </p>
-        <p>Life is good</p>
+        <p>{t.about.lifeIsGood}</p>
         <p>
-          Best,
+          {t.about.best}
           <br />
           Klecianny
         </p>
